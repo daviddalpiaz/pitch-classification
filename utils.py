c67408d28a1520effd0ff2e735cfad9ef0d68da0
David Dalpiaz <dalpiaz2@illinois.edu>
2024-04-17 13:00:29 -0500

    additional utility functions

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from pybaseball import playerid_lookup
+from pybaseball import playerid_lookup, playerid_reverse_lookup
 
 
 # get number of missing values in a data frame
@@ -14,9 +14,19 @@ def get_mlbam_key(last, first):
     return player["key_mlbam"].values[0]
 
 
+# get player name given id
+def get_player_name(id):
+    name_info = playerid_reverse_lookup([id])
+    first = name_info["name_first"][0].title()
+    last = name_info["name_last"][0].title()
+    return first + " " + last
+
+
 # process statcast data
 def process_statcast_data(df):
     df = df[~df["game_type"].isin(["E", "S"])]
+    df = df[~df["pitch_type"].isin(["PO"])]
+    df = df.dropna(subset=["pitch_type"])
     target_column = "pitch_type"
     feature_columns = ["release_speed", "release_spin_rate", "pfx_x", "pfx_z", "stand"]
     metadata_columns = "game_date"
@@ -56,3 +66,36 @@ def check_data():
             print("")
             print(df)
             print("")
+
+
+# function to obtain the pitch mix for a pitcher passed by id
+def get_pitch_mix(pitcher_id):
+    df = pd.read_parquet("data/" + str(pitcher_id) + ".parquet")
+    mix = df["pitch_type"].value_counts() / len(df)
+    return mix
+
+
+# train-test split a data frame then also create X and y variants
+def prepare_data_ml(df):
+
+    # train-test split data based on most recent game
+    df_train, df_test = split_last_game(df)
+    # filter rare pitch types
+    df_train = filter_rare_pitch_types(df_train)
+
+    # create X and y for train
+    X_train = df_train.drop("pitch_type", axis=1)
+    y_train = df_train["pitch_type"]
+
+    # create X and y for test
+    X_test = df_test.drop("pitch_type", axis=1)
+    y_test = df_test["pitch_type"]
+
+    return X_train, y_train, X_test, y_test
+
+
+# get list of pitchers with a learned model
+def get_available_pitcher_ids():
+    files = os.listdir("models")
+    ids = [int(f.replace(".joblib", "")) for f in files if f.endswith(".joblib")]
+    return ids
